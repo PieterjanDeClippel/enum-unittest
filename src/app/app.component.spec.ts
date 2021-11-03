7a32c2876afbeb04f29414c47fb52097922746fc
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2021-11-03 23:21:53 +0100

    Working version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-export enum Color {
-  primary = '#0d6efd',
-  secondary = '#6c757d',
-  success = '#198754',
-  danger = '#dc3545',
-  warning = '#ffc107',
-  info = '#0dcaf0',
-  light = '#f8f9fa',
-  dark = '#212529',
-  body = '#212529',
-  white = '#fff'
-}
-
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,20 +15,40 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'enum-unittest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    
     const app = fixture.componentInstance;
     expect(app.title).toEqual('enum-unittest');
   });
 
-  it('should render title', () => {
+  it(`should have as background #dc3545`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('enum-unittest app is running!');
+    const app = fixture.componentInstance;
+    fixture.whenStable().then(() => {
+      console.log('el', fixture.nativeElement);
+      expect(fixture.nativeElement.querySelector('div').style.background).toEqual('rgb(25, 135, 84)');
+    });
   });
 });
+
+export enum Color {
+  primary = '#0d6efd',
+  secondary = '#6c757d',
+  success = '#dc3545',
+  danger = '#198754',
+  warning = '#ffc107',
+  info = '#0dcaf0',
+  light = '#f8f9fa',
+  dark = '#212529',
+  body = '#212529',
+  white = '#fff'
+}
