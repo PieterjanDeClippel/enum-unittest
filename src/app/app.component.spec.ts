075d32cdcb39729022535e59f86e37de3e35c7f8
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2021-11-03 22:59:31 +0100

    npm test not working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+export enum Color {
+  primary = '#0d6efd',
+  secondary = '#6c757d',
+  success = '#198754',
+  danger = '#dc3545',
+  warning = '#ffc107',
+  info = '#0dcaf0',
+  light = '#f8f9fa',
+  dark = '#212529',
+  body = '#212529',
+  white = '#fff'
+}
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        // { provide: Color, useValue: Color }
+      ]
     }).compileComponents();
   });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color } from './enums/color.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'enum-unittest';
+  colors = Color;
 }
